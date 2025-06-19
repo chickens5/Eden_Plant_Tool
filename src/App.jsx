941b87bf5b081a26645ac0a8442d239b546eab71
gajn9r@umsystem.edu <gajn9r@umsl.edu>
2025-06-18 23:29:32 -0400

    Trying to resolve 404 issue

--- src/App.jsx
+++ src/App.jsx
@@ -252,7 +252,7 @@ export default function NativePlantRecommender() {
                     muted
                     playsInline
                 >
-                    <source src="/bg-vid.mp4" type="video/mp4"/>
+                    <source src="/public/bg-vid.mp4" type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
                 <AnimatedBackground/>
